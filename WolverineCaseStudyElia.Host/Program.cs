0bb54b88cee545e9a9c781d2a6a4f5e3839b6df0
Ken Bonny <github@ken.morethancode.be>
2026-04-14 17:45:06 +0200

    Add Scalar API reference and map ScalarApiReference in development environment

--- WolverineCaseStudyElia.Host/Program.cs
+++ WolverineCaseStudyElia.Host/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Negotiate;
 using EndpointConfiguration = NServiceBus.EndpointConfiguration;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -40,6 +41,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
